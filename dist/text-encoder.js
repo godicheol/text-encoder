9f1fdb0d6407f43e35f2ad8a88677ab52b4851d7
2cheol <godicheol@gmail.com>
2022-07-04 16:30:04 +0900

    1

--- dist/text-encoder.js
+++ dist/text-encoder.js
@@ -7,87 +7,90 @@
         var output = [];
         var len = str.length;
         var i;
-
         for (i = 0; i < len; i++) {
             output[i] = str.charCodeAt(i);
         }
         return output;
     }
-
     exports.fromBytes = function(arr) {
         var output = [];
         var len = arr.length;
         var i;
-
         for (i = 0; i < len; i++) {
             output[i] = String.fromCharCode(arr[i]);
         }
         return output;
     }
-
     exports.toByteArray = function(str) {
-        var output = [], p = 0;
-        for (var i = 0; i < str.length; i++) {
-          var c = str.charCodeAt(i);
-          // NOTE: c <= 0xffff since JavaScript strings are UTF-16.
-          if (c > 0xff) {
-            output[p++] = c & 0xff;
-            c >>= 8;
-          }
-          output[p++] = c;
+        var output = [];
+        var p = 0;
+        var i = 0;
+        var len = str.length;
+        var c;
+        for (i; i < len; i++) {
+            c = str.charCodeAt(i);
+            // NOTE: c <= 0xffff since JavaScript strings are UTF-16.
+            if (c > 0xff) {
+                output[p++] = c & 0xff;
+                c >>= 8;
+            }
+            output[p++] = c;
         }
         return output;
     }
-
     exports.fromByteArray = function(arr) {
         var output = [];
         var len = arr.length;
         var i;
-
         for (i = 0; i < len; i++) {
             output[i] = String.fromCharCode(arr[i]);
         }
         return output;
     }
-
+    exports.toArrayBuffer = function(str) {
+        var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
+        var bufView = new Uint16Array(buf);
+        var i = 0;
+        var len = str.length;
+        for (i; i < len; i++) {
+            bufView[i] = str.charCodeAt(i);
+        }
+        return buf;
+    }
+    exports.fromArrayBuffer = function(buf) {
+        return String.fromCharCode.apply(null, new Uint16Array(buf));
+    }
     exports.toBinary = function(str) {
         var output = [];
         var len = str.length;
         var i;
-
         for (i = 0; i < len; i++) {
             output[i] = str.charCodeAt(i).toString(2);
         }
         return output;
     }
-
     exports.fromBinary = function(arr) {
         var output = [];
         var len = arr.length;
         var i;
-
         for (i = 0; i < len; i++) {
             output[i] = String.fromCharCode(parseInt(arr[i], 2));
         }
         return output;
     }
-
     exports.toHex = function(str) {
         var output = [];
         var len = str.length;
         var i;
-
         for (i = 0; i < len; i++) {
             output[i] = str.charCodeAt(i).toString(16);
         }
         return output;
     }
-
     exports.fromHex = function(arr) {
         var output = [];
         var len = arr.length;
         var i;
-
         for (i = 0; i < len; i++) {
             output[i] = String.fromCharCode(parseInt(arr[i], 16));
         }
@@ -98,30 +101,25 @@
         var output = [];
         var len = str.length;
         var i;
-
         for (i = 0; i < len; i++) {
             output[i] = str.charCodeAt(i);
         }
         return output;
     }
-
     exports.fromDecimal = function(arr) {
         var output = [];
         var len = arr.length;
         var i;
-
         for (i = 0; i < len; i++) {
             output[i] = String.fromCharCode(parseInt(arr[i], 10));
         }
         return output;
     }
-
     exports.toUTF8 = function(str) {
         var output = [];
         var i;
         var len = str.length;
         var ch;
-        
         for (i = 0; i < len; i++) {
             ch = str.charCodeAt(i);
             if (ch < 0x80) {
@@ -144,13 +142,12 @@
         }
         return output;
     }
-
     exports.fromUTF8 = function(arr) {
         var output = [];
         var len = arr.length;
         var i;
-        var n, ch;
-
+        var n;
+        var ch;
         for (i = 0; i < len; i++) {
             n = arr[i];
             if (n < 0x80) {
@@ -170,21 +167,20 @@
         }
         return output;
     }
-
     exports.toUnicode = function(str) {
         var output = [];
-        for (var i = 0; i < str.length; i++) {
+        var i = 0;
+        var len = str.length;
+        for (i; i < len; i++) {
             // Assumption: all characters are < 0xffff
             output[i] = "\\u" + ("000" + str.charCodeAt(i).toString(16)).slice(-4);
         }
         return output;
     }
-
     exports.fromUnicode = function(arr) {
         var output = [];
         var len = arr.length;
         var i;
-
         for (i = 0; i < len; i++) {
             output[i] = String.fromCharCode(parseInt(arr[i].slice(-4), 16));
         }
